81b23f65d733aca3e65dc89214735fd51ae7084d
Onur Yueksek <yueksek.onur@gmail.com>
2023-06-20 13:32:40 +0200

    Almost finished

--- 004-Restful-Backends/004-TodoApp/script.js
+++ 004-Restful-Backends/004-TodoApp/script.js
@@ -8,22 +8,101 @@ const divFilter = document.getElementById("div-filter");
 const removeBTN = document.getElementById("remove-button");
 const inputfield = document.getElementById("input-field");
 
-addBTN.addEventListener("click", () => {});
+addBTN.addEventListener("click", () => {
+  console.log(inputfield.value);
+  getDataFromAPI("POST", 0, "", inputfield.value);
+  inputfield.value = "";
+});
 
 divFilter.addEventListener("click", () => {
   if (document.getElementById("input-all").checked) {
-    // API Get
+    getDataFromAPI();
   }
   if (document.getElementById("input-open").checked) {
-    // API Get =>   done: false,
+    getDataFromAPI(0, 0, 0, 0, "open");
   }
   if (document.getElementById("input-done").checked) {
-    // API Get =>   done: true,
+    getDataFromAPI(0, 0, 0, 0, "done");
   }
 });
 
 removeBTN.addEventListener("click", () => {
-  // API Del done: true
+  for (const child of ul.children) {
+    if (child.children[0].checked) {
+      console.log(child.id);
+      getDataFromAPI("DELETE", child.id);
+      getDataFromAPI();
+    }
+  }
 });
 
-function renderTodos() {}
+function renderTodos(todos) {
+  ul.innerHTML = "";
+
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    const checkBox = document.createElement("input");
+
+    checkBox.type = "checkbox";
+    checkBox.checked = todo.done;
+
+    li.appendChild(checkBox);
+    li.appendChild(document.createTextNode(todo.description));
+    li.setAttribute("id", todo.id);
+    ul.appendChild(li);
+  });
+}
+
+function getDataFromAPI(HttpMethod, id, todos, newTodo, filter) {
+  const URL = "http://localhost:4730/todos";
+
+  if (HttpMethod === "POST") {
+    const todo = { description: newTodo, done: false };
+    fetch(URL, {
+      method: HttpMethod,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    })
+      .then((req) => req.json())
+      .then((todos) => {
+        console.log(todos);
+        renderTodos(todos);
+      });
+  } else if (HttpMethod === "PUT") {
+    //
+  } else if (HttpMethod === "DELETE") {
+    fetch(URL + "/" + id, {
+      method: HttpMethod,
+    })
+      .then((res) => res.json())
+      .then(() => {});
+  } else {
+    if (filter === "open") {
+      fetch(URL)
+        .then((req) => req.json())
+        .then((todos) => {
+          const filteredArray = todos.filter((todos) => todos.done === false);
+          renderTodos(filteredArray);
+        });
+    } else if (filter === "done") {
+      fetch(URL)
+        .then((req) => req.json())
+        .then((todos) => {
+          const filteredArray = todos.filter((todos) => todos.done === true);
+          console.log(filteredArray);
+          renderTodos(filteredArray);
+        });
+    } else {
+      fetch(URL)
+        .then((req) => req.json())
+        .then((todos) => {
+          console.log(todos);
+          renderTodos(todos);
+        });
+    }
+  }
+}
+
+getDataFromAPI();
