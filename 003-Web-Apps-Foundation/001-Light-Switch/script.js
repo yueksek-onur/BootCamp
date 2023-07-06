099d26cdbdcfffc9eba67836a600416247dbabb7
Onur Yueksek <yueksek.onur@gmail.com>
2023-07-07 00:13:43 +0200

    .

--- 003-Web-Apps-Foundation/001-Light-Switch/script.js
+++ 003-Web-Apps-Foundation/001-Light-Switch/script.js
@@ -1,9 +1,6 @@
-const btn = document.getElementById("btn");
-
 btn.addEventListener("click", () => {
   document.body.classList.toggle("body--dark");
-
-  btn.classList.toggle("button-dark");
+  document.getElementById("btn").classList.toggle("button-dark");
 
   document.title === "Good Night"
     ? (document.title = "Good Morning")
